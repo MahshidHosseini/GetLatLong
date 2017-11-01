03c33d4f1e8291a6f12254bb07ad969dd7b5627a
MahshidHosseini <33192812+MahshidHosseini@users.noreply.github.com>
2017-11-01 15:39:33 +0330

    init GetLatLang

---
+++ GetLatLong/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace GetLatLong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.btnGetLatLong = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "To:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-104, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "From:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(364, 20);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(172, 20);
+            this.txtTo.TabIndex = 16;
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(109, 20);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(164, 20);
+            this.txtFrom.TabIndex = 15;
+            // 
+            // btnGetLatLong
+            // 
+            this.btnGetLatLong.Location = new System.Drawing.Point(202, 68);
+            this.btnGetLatLong.Name = "btnGetLatLong";
+            this.btnGetLatLong.Size = new System.Drawing.Size(124, 23);
+            this.btnGetLatLong.TabIndex = 14;
+            this.btnGetLatLong.Text = "Get LatLong";
+            this.btnGetLatLong.UseVisualStyleBackColor = true;
+            this.btnGetLatLong.Click += new System.EventHandler(this.btnGetLatLong_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "From:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 121);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Controls.Add(this.btnGetLatLong);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.Button btnGetLatLong;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GetLatLong/Form1.cs
@@ -0,0 +1,172 @@
+﻿using GetLatLong.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using static GetLatLong.Models.GeoCodeResponse;
+
+namespace GetLatLong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String _filePath = @"D:\Log\LatLongLog\LatLong.txt";
+        private void btnGetLatLong_Click(object sender, EventArgs e)
+        {
+            
+            int from = int.Parse(txtFrom.Text);
+            int to = int.Parse(txtTo.Text);
+
+            MessageBox.Show("Start");
+
+            LatLongEntities1 context = new LatLongEntities1();
+
+            foreach (var result1 in context.Accommodation2.Where(p => p.AccommodationlID >= from && p.AccommodationlID <= to))
+            {
+                using (LatLongEntities1 db = new LatLongEntities1())
+                {
+                    try
+                    {
+
+
+                        long hotelId = result1.AccommodationlID;
+
+                        Accommodation2 hotel = db.Accommodation2.SingleOrDefault(p => p.AccommodationlID == hotelId);
+                        WebClient web = new WebClient();
+                        //if (hotel.Address.Trim() != string.Empty)
+                        //{
+                        //1)string address = hotel.Name+ IsNull(hotel.Address) + IsNull(hotel.Country) + IsNull(hotel.CityName);
+                        //2)string address =hotel.Address+ IsNull(hotel.Country) + IsNull(hotel.CityName);
+                        //3)
+                        string temp = "";
+                        if (hotel.Address != null)
+                            temp = hotel.Address.Trim();
+                        else
+                            temp = hotel.Name;
+                        string address = temp + IsNull(hotel.LocationNameLong);
+
+                            string url = $"http://maps.google.com/maps/api/geocode/xml?address={address} & Sensore=true";
+                            string res = web.DownloadString(url);
+
+                            if (res.Contains("<status>ZERO_RESULTS</status>"))
+                            {
+                                string result = $"AccommodationlID: {hotel.AccommodationlID} With address: {url} = Zero Result";
+                                File.AppendAllLines(_filePath, new string[] { result });
+                                continue;
+                            }
+                            var geoCode = ConvertXMToObject<GeocodeResponse>(res);
+
+                            hotel.Lat = geoCode.result.geometry.location.lat.ToString();
+                            hotel.Lng = geoCode.result.geometry.location.lng.ToString();
+
+                            db.SaveChanges();
+                        }
+               
+
+                    //}
+                    catch (Exception ex)
+                    {
+                        try
+                        {
+
+
+                            long hotelId = result1.AccommodationlID;
+                            Accommodation2 accomodation = context.Accommodation2.SingleOrDefault(p => p.AccommodationlID == hotelId);
+                            string lineError = ex.LineNumber().ToString();
+                            string message = ex.InnerException == null ? ex.Message : ex.InnerException.ToString();
+                            InsertLog(result1.AccommodationlID, "Error", lineError, message);
+                            db.SaveChanges();
+
+
+                        }
+                        catch (Exception)
+                        {
+                            //test
+
+                        }
+
+                    }
+
+                }
+            }
+
+            MessageBox.Show("end");
+          
+
+        }
+        public void InsertLog(long HotelID, string Exception, string line, string ErrorText)
+        {
+            try
+            {
+                var context = new LatLongEntities1();
+                logHotel log = new logHotel();
+                log.HotelID = HotelID;
+                log.exeption = Exception;
+                log.line = line;
+                log.line1 = ErrorText;
+                context.logHotel.Add(log);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        private GeocodeResponse ConvertXMToObject<T>(string xml)
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(typeof(T));
+            using (StringReader rdr = new StringReader(xml))
+            {
+                var xmlResult = (GeocodeResponse)xsSubmit.Deserialize(rdr);
+                return xmlResult;
+            }
+        }
+
+
+        private string IsNull(string input)
+        {
+            string res;
+            if (input == string.Empty)
+                res = " ";
+            else
+                res = ","+input;
+            return res;
+            
+        }
+    }
+
+}
+public static class ExceptionHelper
+{
+    public static int LineNumber(this Exception e)
+    {
+
+        int linenum = 0;
+        try
+        {
+            //linenum = Convert.ToInt32(e.StackTrace.Substring(e.StackTrace.LastIndexOf(":line") + 5));
+
+            //For Localized Visual Studio ... In other languages stack trace  doesn't end with ":Line 12"
+            linenum = Convert.ToInt32(e.StackTrace.Substring(e.StackTrace.LastIndexOf(' ')));
+
+        }
+        catch
+        {
+            //Stack trace is not available!
+        }
+        return linenum;
+    }
+}
+
